31e00f76259a27144e5b05a0ffeca92f03ffcfd3
snoor_madih <snoor.madih@gmail.com>
2021-05-12 09:49:51 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,12 @@
+function Scooter(year,color,model)
+{
+  this.year=year;
+  this.model=model;
+  this.color=color;
+}
+function Driver(name,age,experience)
+{
+  this.name=name;
+  this.age=age;
+  this.experience=experience;
+}
